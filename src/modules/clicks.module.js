278947c35404f7fbf153331217c8ca4e33d40f41
Tatiana <t.albatovskaya@gmail.com>
2021-11-28 15:13:31 +0200

    add clickAnalitycsModule

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,36 @@
 import {Module} from '../core/module'
 
 export class ClicksModule extends Module {
+ 
+    constructor(type, text){
+        super(type, text);
+        this.body= document.querySelector('body');
+        this.clickcount  = 0;
+
+    }
+
+    #enableCountingClicks() {
+        
+       return  ()=> {
+       
+            ++ this.clickcount 
+            
+            setTimeout(  () => { 
+               
+                this.text =  `You clicked ${this.clickcount} times.`
+              //  console.log(this.text);
+                this.body.innerHTML =this.toHTML(this.text)
+               
+        }, 2000);   
+      
+        }   
+       
+    }  
+    init(){
+
+        this.body.addEventListener('click', this.#enableCountingClicks(), false)
+        
+        
+    }
 
 }
